08cebd2c2c6f34d3d8a0ae59908f281e19709801
Ahmad Maulana Renhoran <ahmad@seleris.id>
2026-04-09 15:18:56 +0700

    Initial Setup

---
+++ src/models/user.model.ts
@@ -0,0 +1,8 @@
+import { pgTable, serial, varchar, text } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  password: text('password').notNull(),
+  name: varchar('name', { length: 255 }).notNull(),
+});
