84020c2e39388790bcdcac053d17a4a3277475fb
Ahmad Maulana Renhoran <ahmad@seleris.id>
2026-04-28 15:34:11 +0700

    github actions

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api/v1', v1Routes);
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 7860;
 app.listen(PORT, () => {
     console.log("Berhasil connect bruhhh")
 });
