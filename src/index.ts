48d0b1d43893b4256e8ccc11e4bafdc1601505db
Ahmad Maulana Renhoran <ahmad@seleris.id>
2026-04-29 10:00:32 +0700

    fix route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import app from './app';
 const PORT = process.env.PORT ? Number(process.env.PORT) : 7860;
 const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
 });
