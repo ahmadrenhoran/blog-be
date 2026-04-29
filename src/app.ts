22267c7b3493c7022bc2621ea876518e127a944b
Ahmad Maulana Renhoran <ahmad@seleris.id>
2026-04-29 10:05:31 +0700

    debugging

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,12 @@ import { errorHandler } from './middleware/error.middleware';
 dotenv.config();
 
 const app = express();
-
-app.use(cors()); 
-app.use(express.json()); 
+app.use((req, res, next) => {
+  console.log("🔥 HIT:", req.method, req.url);
+  next();
+});
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.json({
